0c0a5567e063fd00f3cb82ee2786c130c5843e05
DKlaver <dklaver15@gmail.com>
2021-03-25 18:53:01 -0500

    Initial commit.

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    height: {
+      75: "75vh",
+    },
+    minHeight: {
+      250: "250px",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/line-clamp"), require("@tailwindcss/forms")],
+};
