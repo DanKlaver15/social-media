e297abd54428f68b49f1f8ed85e03f7d2f656ae1
DKlaver <dklaver15@gmail.com>
2021-03-24 21:06:00 -0500

    Added posts

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const users = require("./routes/user");
+const users = require("./routes/users");
+const posts = require("./routes/posts");
 
 const app = express();
 
@@ -10,6 +11,7 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 app.use("/api/users", users);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
