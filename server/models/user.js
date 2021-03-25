9c5566b37fe492507dc214923328491c8b0d4c5a
DKlaver <dklaver15@gmail.com>
2021-03-24 21:07:06 -0500

    Renamed users to user to differentiate between files in routes folder.

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ const { requestSchema } = require("./request");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, minlength: 3, trim: true },
-  password: { type: String, required: true, minlength: 8, trim: true },
+  password: { type: String, required: true, minlength: 3, trim: true },
   email: {
     type: String,
     required: true,
@@ -11,12 +11,15 @@ const userSchema = new mongoose.Schema({
     trime: true,
     lowercase: true,
   },
+  firstName: { type: String, required: true, minlength: 1, trim: true },
+  lastName: { type: String, required: true, minlength: 1, trim: true },
   avatar: { type: String },
   bgImage: { type: String },
   bio: { type: String, minlength: 5, trim: true },
   friends: [requestSchema],
   theme: { type: String, default: "light" },
   online: { type: Boolean, default: false },
+  registered: { type: Date, default: Date.now },
 });
 
 const User = mongoose.model("User", userSchema);
