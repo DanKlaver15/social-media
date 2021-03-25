bdbdeec6063f029d3191bb96be2701c24e0957fd
DKlaver <dklaver15@gmail.com>
2021-03-24 20:04:12 -0500

    Initial commit.

---
+++ server/models/users.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, minlength: 3, trim: true },
+  password: { type: String, required: true, minlength: 8, trim: true },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    trime: true,
+    lowercase: true,
+  },
+  avatar: { type: String },
+  bgImage: { type: String },
+  bio: { type: String, minlength: 5, trim: true },
+  friends: [mongoose.Types.ObjectId],
+  theme: { type: String, default: "light" },
+  online: { type: Boolean, default: false },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
