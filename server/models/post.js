6b6b08d54327ccbacc87271db2f1bfa3372cdd20
DKlaver <dklaver15@gmail.com>
2021-03-24 20:10:58 -0500

    Initial commit.

---
+++ server/models/post.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  content: { type: String, minlength: 1 },
+  date: { type: Date, default: Date.now },
+  userId: { type: mongoose.Types.ObjectId },
+  likes: { type: Number, default: 0 },
+  replies: [replySchema],
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
