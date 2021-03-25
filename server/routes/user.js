6a655b33432cf3e5f1d6ff5b056ad53d17eb9ece
DKlaver <dklaver15@gmail.com>
2021-03-24 21:06:39 -0500

    Renamed user to users to differentiate between files in models folder.

--- server/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const Joi = require("joi");
-
-router.get("/", async (req, res) => {
-  return res.status(200).end();
-});
-
-module.exports = router;
---
+++ server/routes/users.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const Joi = require("joi");
+const userController = require("../controllers/userController");
+
+router.route("/").get(userController.getAll).post(userController.createOne);
+router
+  .route("/:id")
+  .get(userController.getOne)
+  .put(userController.updateOne)
+  .delete(userController.removeOne);
+
+module.exports = router;
