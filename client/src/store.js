6d733eb36d7020a2a6e9d14bb8b3cbaf9a0c8898
DKlaver <dklaver15@gmail.com>
2021-03-25 20:57:30 -0500

    Imported reducers.

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import { authUser, user } from "./User/reducers";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const reducers = {};
+const reducers = {
+  authUser,
+  user,
+};
 
 const rootReducer = combineReducers(reducers);
 
