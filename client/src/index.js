7ed75c631195549962ba8644ff7025bb3df5aca1
DKlaver <dklaver15@gmail.com>
2021-03-25 21:32:37 -0500

    Added switch and route components.

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { configureStore } from "./store";
+import UserLoginForm from "./components/userLoginForm";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <Provider store={configureStore()}>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/" component={App}></Route>
+        {/* <Route path="/login" component={UserLoginForm}></Route> */}
+      </Switch>
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
