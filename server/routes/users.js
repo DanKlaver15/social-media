5b60f01ac28ac4bdd04eb73eb22a83ecb42c3329
DKlaver <dklaver15@gmail.com>
2021-03-28 14:05:41 -0500

    Imported friends controller and added route for friends.

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
 const userController = require("../controllers/userController");
+const friendsController = require("../controllers/friendsController");
 const auth = require("../middlewares/auth");
 
 router.route("/").get(userController.getAll).post(userController.createOne);
@@ -13,5 +14,5 @@ router
   .get(userController.getOne)
   .put(userController.updateOne)
   .delete(userController.removeOne);
-
+router.route("/:id/friends").get(auth, friendsController.getAll);
 module.exports = router;
