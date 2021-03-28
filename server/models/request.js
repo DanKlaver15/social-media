09c29d3af9291f4379595a016465ed19f1641b24
DKlaver <dklaver15@gmail.com>
2021-03-28 14:05:24 -0500

    Revised request model.

--- server/models/request.js
+++ server/models/request.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
 const requestSchema = new mongoose.Schema({
-  senderId: { type: mongoose.Types.ObjectId },
+  sender: {
+    senderId: { type: mongoose.Types.ObjectId },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    avatar: { type: String },
+  },
   accepted: { type: Boolean, default: false },
 });
 
