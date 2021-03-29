470c7e137c0f0780312f3d399cc52ebaa717c0a9
DKlaver <dklaver15@gmail.com>
2021-03-28 20:27:12 -0500

    Imported online controller and added new route to get the users online.

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,16 +3,20 @@ const router = express.Router();
 const Joi = require("joi");
 const userController = require("../controllers/userController");
 const friendsController = require("../controllers/friendsController");
+const onlineController = require("../controllers/onlineController");
 const auth = require("../middlewares/auth");
 
 router.route("/").get(userController.getAll).post(userController.createOne);
 router.route("/login").post(userController.login);
 router.route("/auth").post(auth, userController.authorize);
+router.route("/online").get(onlineController.getAll); // add auth
 
 router
   .route("/:id")
   .get(userController.getOne)
   .put(userController.updateOne)
   .delete(userController.removeOne);
+
 router.route("/:id/friends").get(auth, friendsController.getAll);
+
 module.exports = router;
