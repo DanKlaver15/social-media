c657118495f0aa5da355d3d4bfd432223ed06894
DKlaver <dklaver15@gmail.com>
2021-03-30 18:50:37 -0500

    Initial commit.

---
+++ server/middlewares/online.js
@@ -0,0 +1,20 @@
+const Online = require("../models/online");
+
+const online = () => async (req, res, next) => {
+  const onlineUsers = await query.getAll(Online);
+  const foundUser = onlineUsers.find((onlineUser) => onlineUser === res._id);
+
+  if (!foundUser) {
+    onlineUsers.push(user._id);
+
+    try {
+      onlineUsers.save();
+
+      return next();
+    } catch (err) {
+      return res.status(400).send({ message: "Unable to " });
+    }
+  }
+};
+
+module.exports = online;
