2999adaa4c0dc1e6a4df1d2a2888a3755cf93f0c
Lee Stratman <stratmanlc10@gmail.com>
2021-03-30 19:48:59 -0500

    Add getFeed conroller function.

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,4 +1,17 @@
 const Post = require("../models/post");
+const query = require("../utils/query");
 const crudController = require("../utils/crud");
 
-module.exports = crudController(Post);
+const getFeed = async (req, res) => {
+  try {
+    const feed = await Post.find({ userId: req.params.id }).sort({ date: -1 });
+
+    if (!feed) return res.status(401).send({ message: "Feed unavailable" });
+
+    return res.status(201).send(feed);
+  } catch (err) {
+    return res.status(500).send({ message: err });
+  }
+};
+
+module.exports = { ...crudController(Post), getFeed };
