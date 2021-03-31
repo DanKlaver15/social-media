b3e31e2a1dc1b7a1ad3346d6d3daab5fa6ef641f
Lee Stratman <stratmanlc10@gmail.com>
2021-03-31 17:56:34 -0500

    Remove online model.

--- server/controllers/onlineController.js
+++
@@ -1,4 +0,0 @@
-const UsersOnline = require("../models/online");
-const crudController = require("../utils/crud");
-
-module.exports = crudController(UsersOnline);
--- server/models/online.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const onlineSchema = new mongoose.Schema({
-  usersOnline: [mongoose.Types.ObjectId],
-});
-
-const UsersOnline = mongoose.model("UsersOnline", onlineSchema);
-
-module.exports = UsersOnline;
