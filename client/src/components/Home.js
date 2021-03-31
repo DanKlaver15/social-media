1f8358442c2eb60bb7d6aa9bb2cf15a8d75d6606
Lee Stratman <stratmanlc10@gmail.com>
2021-03-30 19:49:25 -0500

    Update Home screen to display feed and AddPostForm.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,99 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import AddPostForm from "./forms/AddPostForm";
+import Avatar from "./Avatar";
+import { getFeedRequest } from "../state/Post/thunks";
+import Feed from "./Feed";
 
-const Home = () => {
-  return <div>Dashboard</div>;
+const Home = ({ user, getFeed, feed }) => {
+  useEffect(() => {
+    getFeed(user._id);
+  });
+  return (
+    <>
+      <div className="-m-8 mb-4">
+        <div>
+          <img
+            className="h-32 w-full object-cover lg:h-48"
+            src="https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+            alt=""
+          />
+        </div>
+        <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+            <div className="flex">
+              <Avatar size={32} source={user.avatar} />
+            </div>
+            <div className="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+              <div className="sm:hidden md:block mt-6 min-w-0 flex-1">
+                <h1 className="text-2xl font-bold text-gray-900 truncate">
+                  {`${user.firstName} ${user.lastName}`}
+                </h1>
+              </div>
+              <div className="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
+                <button
+                  type="button"
+                  className="inline-flex justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+                >
+                  <svg
+                    className="-ml-1 mr-2 h-5 w-5 text-gray-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                  </svg>
+                  <span>Message</span>
+                </button>
+                <button
+                  type="button"
+                  className="inline-flex justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+                >
+                  <svg
+                    className="-ml-1 mr-2 h-5 w-5 text-gray-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                  </svg>
+                  <span>Call</span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="hidden sm:block md:hidden mt-6 min-w-0 flex-1">
+            <h1 className="text-2xl font-bold text-gray-900 truncate">
+              Ricardo Cooper
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-50 px-4 py-6 sm:px-6">
+        <div className="flex space-x-3">
+          <div className="flex-shrink-0">
+            <Avatar size={10} source={user.avatar} />
+          </div>
+          <div className="min-w-0 flex-1">
+            <AddPostForm />
+            <Feed />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default Home;
+const mapStateToProps = (state) => ({
+  user: state.user,
+  feed: state.feed,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getFeed: (userId) => dispatch(getFeedRequest(userId)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
