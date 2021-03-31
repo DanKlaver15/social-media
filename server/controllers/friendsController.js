fe74422129ae36c18ce6cb36560575d59480f0a7
DKlaver <dklaver15@gmail.com>
2021-03-30 19:21:03 -0500

    Added functions to determine which friends are online.

--- server/controllers/friendsController.js
+++ server/controllers/friendsController.js
@@ -1,11 +1,29 @@
 const User = require("../models/user");
 const query = require("../utils/query");
+const Online = require("../models/online");
 
 const getAll = async (req, res) => {
   try {
     const user = await query.getOne(User, req.params.id);
 
     if (!user) return res.status(401).send({ message: "Invalid user" });
+
+    const friendIds = user.friends.map((friend) => {
+      return friend.senderId;
+    });
+
+    const onlineFriends = await Online.find({
+      usersOnline: { $in: friendsIds },
+    });
+
+    const friends = user.friends.map((friend) => {
+      if (onlineFriends.includes(friend.senderId)) {
+        return { ...friend, online: true };
+      } else {
+        return { ...friend, online: false };
+      }
+    });
+
     return res.status(200).send(user.friends);
   } catch (err) {
     return res.status(500).send({ message: `Server Error: ${err}` });
