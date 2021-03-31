d4c1971be2c12a6319697f33815ba97d02b80cee
Lee Stratman <stratmanlc10@gmail.com>
2021-03-31 18:02:02 -0500

    Update friends controller to use the new user model.

--- server/controllers/friendsController.js
+++ server/controllers/friendsController.js
@@ -1,6 +1,5 @@
 const User = require("../models/user");
 const query = require("../utils/query");
-const Online = require("../models/online");
 
 const getAll = async (req, res) => {
   try {
@@ -8,27 +7,45 @@ const getAll = async (req, res) => {
 
     if (!user) return res.status(401).send({ message: "Invalid user" });
 
-    const friendsIds = user.friends.map((friend) => {
-      return friend.senderId;
+    const friends = await User.find({
+      _id: { $in: user.friends },
+    }).select({
+      username: 1,
+      email: 1,
+      firstName: 1,
+      lastName: 1,
+      avatar: 1,
+      online: 1,
     });
 
-    const onlineFriends = await Online.find({
-      usersOnline: { $in: friendsIds },
-    });
-
-    const friends = user.friends.map((friend) => {
-      if (onlineFriends.includes(friend.senderId)) {
-        return { ...friend, online: true };
-      } else {
-        return { ...friend, online: false };
-      }
-    });
-
-    return res.status(200).send(user.friends);
+    return res.status(200).send(friends);
   } catch (err) {
     console.log(err);
     return res.status(500).send({ message: `Server Error: ${err}` });
   }
 };
 
-module.exports = { getAll };
+const deleteFriend = async (req, res) => {
+  try {
+    const user = await query.getOne(User, req.params.id);
+
+    if (!user) return res.status(401).send({ message: "Invalid user" });
+
+    const friend = await query.getOne(User, req.params.friendId);
+
+    if (!friend) return res.status(401).send({ message: "Invalid friend" });
+
+    user.friends = user.friends.filter((friendId) => friendId === friend._id);
+    friend.friends = friend.friends.filter((friendId) => friendId === user._id);
+
+    await user.save();
+    await friend.save();
+
+    return res.status(200).send({ message: "Friend deleted" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ message: `Server Error: ${err}` });
+  }
+};
+
+module.exports = { getAll, deleteFriend };
