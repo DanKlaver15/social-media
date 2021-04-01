da15dc8bc9485f2c7cc6b7dae87e9d4ec754f56e
Lee Stratman <stratmanlc10@gmail.com>
2021-03-31 20:03:15 -0500

    Reduce props sent to PeopleListItem.

--- client/src/components/PeopleListItem.js
+++ client/src/components/PeopleListItem.js
@@ -2,30 +2,35 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Avatar from "./Avatar";
 
-const PeopleListItem = ({ id, name, email, avatar }) => (
+const PeopleListItem = ({ person }) => (
   <li className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
     <div className="w-full flex items-center justify-between p-6 space-x-6">
       <div className="flex-1 truncate">
         <div className="flex items-center space-x-3">
-          <h3 className="text-gray-900 text-sm font-medium truncate">{name}</h3>
+          <h3 className="text-gray-900 text-sm font-medium truncate">{`${person.firstName} ${person.lastName}`}</h3>
         </div>
-        <p className="mt-1 text-gray-500 text-sm truncate">{email}</p>
+        <p className="mt-1 text-gray-500 text-sm truncate">{person.email}</p>
       </div>
-      <Avatar size={10} source={avatar} />
+      <Avatar size={10} source={person.avatar} />
     </div>
     <div>
       <div className="-mt-px flex divide-x divide-gray-200">
         <div className="w-0 flex-1 flex">
           <Link
-            to={`/person/${id}`}
+            to={`/person/${person._id}`}
             className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
           >
             View
           </Link>
         </div>
         <div className="-ml-px w-0 flex-1 flex">
-          <button className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
-            Add as Friend
+          <button
+            onClick={() => {
+              console.log("Add friend!");
+            }}
+            className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
+          >
+            {person.friends === "no" ? "Add Friend" : "Already Friends"}
           </button>
         </div>
       </div>
