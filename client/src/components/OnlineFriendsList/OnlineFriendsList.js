1c9970b821c8a258ee18191719cf9c63ed8c146a
DKlaver <dklaver15@gmail.com>
2021-04-02 15:22:02 -0500

    Changed the button to a div with an image so it is no longer clickable.

--- client/src/components/OnlineFriendsList/OnlineFriendsList.js
+++ client/src/components/OnlineFriendsList/OnlineFriendsList.js
@@ -84,7 +84,7 @@ const OnlineFriendsList = ({ friends, friendsLoading, getFriends, user }) => {
                     className="border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-10 sm:text-sm"
                   />
                 </div>
-                <button className="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+                <div className="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
                   <svg
                     className="h-5 w-5 text-gray-400"
                     xmlns="http://www.w3.org/2000/svg"
@@ -98,7 +98,7 @@ const OnlineFriendsList = ({ friends, friendsLoading, getFriends, user }) => {
                       clipRule="evenodd"
                     />
                   </svg>
-                </button>
+                </div>
               </div>
             </div>
           </div>
