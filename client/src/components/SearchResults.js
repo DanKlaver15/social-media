cc79863af25bf8ae70a9ca3d40c8f42b410d6bdf
DKlaver <dklaver15@gmail.com>
2021-04-03 22:13:26 -0500

    Added a loading animation.

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,9 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import PeopleList from "./PeopleList";
 
-const SearchResults = ({ results }) => {
-  return results && results.length > 0 ? (
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > * + *": {
+      marginLeft: theme.spacing(2),
+    },
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+  },
+}));
+
+const SearchResults = ({ results, inProgress }) => {
+  const classes = useStyles();
+
+  return inProgress ? (
+    <Backdrop className={classes.root}>
+      <CircularProgress color="white" />
+    </Backdrop>
+  ) : results && results.length > 0 ? (
     <PeopleList people={results} />
   ) : (
     <div className="dark:text-gray-400">No results found.</div>
@@ -12,6 +34,7 @@ const SearchResults = ({ results }) => {
 
 const mapStateToProps = (state) => ({
   results: state.searchResults,
+  inProgress: state.searchInProgress,
 });
 
 export default connect(mapStateToProps)(SearchResults);
