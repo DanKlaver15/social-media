0cbc205ad43c540941bebed9a51060f4bb12394e
Lee Stratman <stratmanlc10@gmail.com>
2021-04-05 20:15:39 -0500

    Replace stock image with avatar for mobile view.

--- client/src/components/AuthenticatedApp.js
+++ client/src/components/AuthenticatedApp.js
@@ -5,6 +5,7 @@ import AppSearch from "./AppSearch";
 import { Link } from "react-router-dom";
 import { UserAdd } from "./Icons/icons";
 import { connect } from "react-redux";
+import Avatar from "./Avatar";
 
 const AuthenticatedApp = ({ logout, children, user }) => {
   const darkOrLight = () => {
@@ -239,11 +240,7 @@ const AuthenticatedApp = ({ logout, children, user }) => {
             <div className="border-t border-gray-200 pt-4 pb-3">
               <div className="max-w-8xl mx-auto px-4 flex items-center sm:px-6">
                 <div className="flex-shrink-0">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixqx=sZQjNUjFo7&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
+                  <Avatar size={10} source={user.avatar} />
                 </div>
                 <div className="ml-3 min-w-0 flex-1">
                   <div className="text-base font-medium text-gray-800 truncate">
