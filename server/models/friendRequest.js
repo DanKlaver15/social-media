f03c71893463f0a559de38b8cb41fb439b9308d9
Lee Stratman <stratmanlc10@gmail.com>
2021-04-05 20:24:00 -0500

    Update friendRequest model to reference User model.

--- server/models/friendRequest.js
+++ server/models/friendRequest.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const requestSchema = new mongoose.Schema({
-  senderId: { type: mongoose.Types.ObjectId, required: true },
-  receiverId: { type: mongoose.Types.ObjectId, required: true },
+  senderId: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  receiverId: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
   accepted: { type: Boolean, default: false },
   date: { type: Date, default: Date.now },
 });
