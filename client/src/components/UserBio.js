b4248c6bba6babee98f2aa2e5b9dfb4e21165018
Lee Stratman <stratmanlc10@gmail.com>
2021-04-08 12:02:37 -0500

    Add UserBio component.

--- client/src/components/UserBio.js
+++ client/src/components/UserBio.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const UserBio = ({ bio }) => (
-  <div class="bg-white px-4 py-6 shadow sm:rounded-lg sm:px-6">
-    <div class="sm:flex sm:justify-between sm:items-baseline">
-      <h3 class="text-base font-medium">About</h3>
+  <div className="bg-white px-4 py-6 shadow sm:rounded-lg sm:px-6">
+    <div className="sm:flex sm:justify-between sm:items-baseline">
+      <h3 className="text-base font-medium">About</h3>
     </div>
-    <div class="mt-4 space-y-6 text-sm text-gray-800">{bio}</div>
+    <div className="mt-4 space-y-6 text-sm text-gray-800">{bio}</div>
   </div>
 );
 
